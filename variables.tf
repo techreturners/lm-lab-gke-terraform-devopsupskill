86378265cabd6b7535736f409d592131fbb8b12a
James Heggs <james.heggs@techreturners.com>
2023-02-07 21:55:15 +0000

    fix: Remove unused vars

--- variables.tf
+++ variables.tf
@@ -6,16 +6,6 @@ variable "region" {
   description = "region"
 }
 
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
 variable "gke_num_nodes" {
   default     = 1
   description = "number of gke nodes"
